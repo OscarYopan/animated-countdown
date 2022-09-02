8a79e4f401a523c9d8f41e179650c3539dfd9d50
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-09-02 18:06:22 -0300

    --

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const nums = document.querySelectorAll('.nums span')
 const counter = document.querySelector('.counter')
-const finalMessenge = document.querySelector('.final')
+const finalMessage = document.querySelector('.final')
 const replayBtn = document.querySelector('#replay')
  
 runAnimation()
 
 function resetDom ()  {
   counter.classList.remove('hide')
-  finalMessenge.classList.remove('.show')
+  finalMessage.classList.remove('.show')
 
   nums.forEach((num) => {
     num.classList.value = ''
@@ -18,7 +18,7 @@ function resetDom ()  {
 
 function runAnimation() {
   nums.forEach((num, idx) => {
-    const nextToLast = num.length - 1
+    const nextToLast = nums.length - 1
 
     num.addEventListener('animationend', (e) => {
       if (e.animationName === 'goIn' && idx !== nextToLast) {
@@ -28,7 +28,7 @@ function runAnimation() {
         num.nextElementSibling.classList.add('in')
       } else {
         counter.classList.add('hide')
-        finalMessenge.classList.add('show')
+        finalMessage.classList.add('show')
       }
     })
   })
